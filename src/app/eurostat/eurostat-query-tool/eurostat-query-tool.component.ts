491b4bb9ba8f440104552464897fb029f93b4748
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-08-13 13:37:47 +0300

    Query tool emits a get query

--- src/app/eurostat/eurostat-query-tool/eurostat-query-tool.component.ts
+++ src/app/eurostat/eurostat-query-tool/eurostat-query-tool.component.ts
@@ -1,4 +1,10 @@
-import { ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnDestroy,
+  OnInit,
+  Output,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Store } from '@ngrx/store';
 import { AppState, selectCurrentMetadata } from 'src/app/state';
@@ -14,6 +20,7 @@ import { PillSetComponent } from 'src/app/ui/pill-set/pill-set.component';
   styleUrls: ['./eurostat-query-tool.component.scss'],
 })
 export class EurostatQueryToolComponent implements OnInit, OnDestroy {
+  @Output() eurostatQuery = new EventEmitter<string>();
   subscription: Subscription | undefined;
   currentMetadata$ = this.store.select(selectCurrentMetadata);
   ds: any;
@@ -27,22 +34,32 @@ export class EurostatQueryToolComponent implements OnInit, OnDestroy {
     pills: { id: string; tooltip: string }[];
   }[] = [];
   multipleSelection = false;
+  querySelections: { [key: string]: string[] } = {};
 
   query: { dimension: string; selections: string[] }[] = [];
+  labelToId: { [key: string]: string } = {};
+  pillIdToTooltip: { [key: string]: string } = {};
 
-  constructor(
-    private changeDetectorRef: ChangeDetectorRef,
-    private store: Store<AppState>
-  ) {}
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.subscription = this.currentMetadata$.subscribe((metadata) => {
       if (metadata) {
         // nothitng selected from my datasets
         this.ds = JSONstat(metadata);
+        console.log(this.ds);
         const dimensions = this.ds.id ?? [];
         this.dimensions = dimensions;
-        console.log(dimensions);
+
+        // keep a reverse lookup from label to id
+        this.labelToId = dimensions.reduce(
+          (acc: any, dim: any) => ({
+            ...acc,
+            [this.ds.Dimension(dim).label]: dim,
+          }),
+          {}
+        );
+        console.log(this.labelToId);
 
         this.label = this.ds.label;
         this.source = this.ds.source;
@@ -52,6 +69,22 @@ export class EurostatQueryToolComponent implements OnInit, OnDestroy {
         this.dimensionInfos = dimensions.map((dim: string) =>
           this.dimensionInfo(dim)
         );
+
+        // keep a reverse lookup from pill id to tooltip
+        this.pillIdToTooltip = this.dimensionInfos.reduce(
+          (acc: any, dimInfo: any) => ({
+            ...acc,
+            ...dimInfo.pills.reduce(
+              (acc2: any, pill: any) => ({
+                ...acc2,
+                [pill.id]: pill.tooltip,
+              }),
+              {}
+            ),
+          }),
+          {}
+        );
+        console.log(this.pillIdToTooltip);
       }
     });
   }
@@ -78,19 +111,30 @@ export class EurostatQueryToolComponent implements OnInit, OnDestroy {
     return { label, pills };
   }
 
-  onPillSelection(selected: string[]) {
-    this.changeDetectorRef.detectChanges();
-    console.log('onPillSelection', selected);
+  onPillSelection(dimensionId: string, selected: string[]) {
+    const revDimId = this.labelToId[dimensionId];
+    const revSelected = selected.map((s) => this.pillIdToTooltip[s]);
+    this.querySelections[revDimId] = revSelected;
+
+    // Delete values of length 0
+    Object.keys(this.querySelections).forEach(
+      (key) =>
+        this.querySelections[key].length < 1 && delete this.querySelections[key]
+    );
+    console.log('onPillSelection', this.querySelections);
   }
 
   onCheckboxChange(event: Event) {
     this.multipleSelection = (event.target as HTMLInputElement).checked;
   }
 
-  // onDimensionSelections($event: Pill[]) {
-  //   const dimension = $event[0].tooltip ?? '';
-  //   const selections = $event.map((pill) => pill.id ?? '');
-  //   this.query.push({ dimension, selections });
-  //   console.log('onDimensionSelection', this.query);
-  // }
+  onClick() {
+    // Form a query string from the query selections
+    const queryString = Object.entries(this.querySelections)
+      .map(([key, values]) =>
+        values.map((value) => `${key}=${encodeURIComponent(value)}`).join('&')
+      )
+      .join('&');
+    this.eurostatQuery.emit(queryString);
+  }
 }
