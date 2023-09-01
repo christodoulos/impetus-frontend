8f58e9fef59e661e7632b7acf8074ab58fb32b91
Christodoulos Fragkoudakis <christodoulos.fragkoudakis@gmail.com>
2023-09-01 18:03:43 +0300

    pillset keeps pills sorted

--- src/app/ui/pill-set/pill-set.component.ts
+++ src/app/ui/pill-set/pill-set.component.ts
@@ -2,24 +2,34 @@ import {
   Component,
   EventEmitter,
   Input,
+  OnChanges,
   Output,
-  QueryList,
-  ViewChildren,
+  SimpleChanges,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 import { PillComponent } from '../pill/pill.component';
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'pill-set',
   standalone: true,
-  imports: [CommonModule, PillComponent],
+  imports: [CommonModule, FormsModule, PillComponent],
   templateUrl: './pill-set.component.html',
   styleUrls: ['./pill-set.component.scss'],
 })
-export class PillSetComponent {
+export class PillSetComponent implements OnChanges {
   @Input() pills: { id: string; tooltip: string }[] = [];
+  @Input() selectOptions: boolean = false;
+  selectAll: boolean = false;
+  selectAllUuid = uuidv4(); // This is used to make the select all checkbox unique
 
-  // @Input() multipleSelection: boolean = false;
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes['pills']) {
+      this.pills.sort((a, b) => a.id.localeCompare(b.id));
+    }
+  }
+
   private _multipleSelection: boolean = false;
   @Input()
   set multipleSelection(value: boolean) {
@@ -47,6 +57,15 @@ export class PillSetComponent {
       }
     } else {
       this.selectedPills = this.selectedPills.filter((id) => id !== event.id);
+    }
+    this.selectedPillsChange.emit(this.selectedPills);
+  }
+
+  selectAllPills() {
+    if (this.selectAll) {
+      this.selectedPills = this.pills.map((pill) => pill.id);
+    } else {
+      this.selectedPills = [];
     }
     this.selectedPillsChange.emit(this.selectedPills);
   }
